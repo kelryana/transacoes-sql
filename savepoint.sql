c8a4c227fc1345d390b05f6ca31345dae711a47c
Ana Kelry <anakelry346@gmail.com>
2026-03-09 18:08:41 -0300

    Update savepoint.sql

--- savepoint.sql
+++ savepoint.sql
@@ -3,6 +3,14 @@ BEGIN;
   
   SAVEPOINT ponto1;
   
-  UPDATE contas SET saldo = saldo - 100 WHERE nome = 'Alice'; --simulação de erro
-
+  UPDATE contas SET saldo = saldo - 100 WHERE nome = 'Alice';
+  
+  --  erramos (foi de proposito ok) a segunda operação foi errada, volta  até o savepoint
   ROLLBACK TO SAVEPOINT ponto1;
+  
+  -- A primeira operação ainda vale
+  UPDATE contas SET saldo = saldo + 50 WHERE nome = 'Bob'; -- atualizamos 
+  
+COMMIT;
+
+SELECT * FROM contas;
